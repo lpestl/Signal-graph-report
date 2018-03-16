eb376c1a7fd9c59f38b274eede9173bfaaab4f38
kataev.m <lpestlname@gmail.com>
2018-03-16 11:35:03 +0300

    Def

--- Signal-graph.Core/ReportDataViewModel.cpp
+++ Signal-graph.Core/ReportDataViewModel.cpp
@@ -4,4 +4,12 @@
 
 SignalgraphCore::ReportDataViewModel::ReportDataViewModel()
 {
+	dateTime = gcnew String(DateTime::Now.ToString());
+	codeName = gcnew String("XXX-XXX");
+}
+
+SignalgraphCore::ReportDataViewModel::~ReportDataViewModel()
+{
+	if (!codeName) delete codeName;
+	if (!dateTime) delete dateTime;
 }
--- Signal-graph.Core/ReportDataViewModel.h
+++ Signal-graph.Core/ReportDataViewModel.h
@@ -1,12 +1,20 @@
 #pragma once
 #include "BaseViewModel.h"
+#include "ReportData.h"
 
 using namespace System;
 
 namespace SignalgraphCore {
 	public ref class ReportDataViewModel : BaseViewModel
 	{
+	private:
+		ReportData _reportData;
+		String ^dateTime;
+		String ^codeName;
+
 	public:
 		ReportDataViewModel();
+		~ReportDataViewModel();
+
 	};
 }
